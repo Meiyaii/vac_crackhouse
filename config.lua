9cb6e6853caa3113b47c6fa73c4072d537f65729
Simon <59517672+Vacsson@users.noreply.github.com>
2020-01-07 23:29:28 +0100

    Added Config.Door in and out

--- config.lua
+++ config.lua
@@ -4,10 +4,19 @@ Config              = {}
 Config.DrawDistance = 100.0
 Config.WeedSize     = {x = 6.0, y = 6.0, z = 6.0}
 Config.KokainSize   = {x = 2.0, y = 2.0, z = 2.0}
+Config.DoorSize 	= {x = 2.0, y = 2.0, z = 2.0}
 Config.MarkerColor  = {r = 0, g = 0, b = 0}
 Config.MarkerType   = 22
 Config.smokeTime	= 29000
 Config.sniffTime    = 29000
+
+Config.DoorOutSide = {
+	{x= 1200.92, y= -575.62, z= 69.14},
+}
+
+Config.DoorInSide = {
+	{x= 266.05, y= -1007.32, z= -101.01},
+}
 
 Config.Weed = {
 	{x= 259.22, y= -995.93, z= -99.01},
